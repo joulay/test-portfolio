f0c75b2ad6c5795e9c7ffc62e281df4ce1d709db
Julie K <julieskim160@gmail.com>
2018-05-08 11:07:25 -0700

    rough draft

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h2 className="App-title">hello</h2>
+          <p>I'm a web developer based in Los Angeles</p>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
       </div>
     );
   }
 }
 
 export default App;
+
+        // <p className="intro">
+        //   To get started, edit <code>src/App.js</code> and save to reload.
+        // </p>
